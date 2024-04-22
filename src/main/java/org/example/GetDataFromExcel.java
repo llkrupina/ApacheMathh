7eece13119695067d4ecd06252694f4976aa58b1
llkrupina <kwaifare@mail.ru>
2024-04-22 12:15:19 +0300

    ten

--- src/main/java/org/example/GetDataFromExcel.java
+++ src/main/java/org/example/GetDataFromExcel.java
@@ -14,16 +14,13 @@ public class GetDataFromExcel {
             Sheet sheet = workbook.getSheet(sheetName);
 
             if (sheet == null) {
-                //System.out.println("Лист не найден.");
                 JOptionPane.showMessageDialog(null, "Лист не найден", "Ошибка", JOptionPane.ERROR_MESSAGE);
-
                 return null;
             }
 
             int rows = sheet.getPhysicalNumberOfRows();
             if (rows < 2) {
                 JOptionPane.showMessageDialog(null, "Лист пустой или содержит только заголовок", "Ошибка", JOptionPane.ERROR_MESSAGE);
-
                 return null;
             }
 
@@ -32,28 +29,36 @@ public class GetDataFromExcel {
 
             double[][] data = new double[cols][rows - 1]; // Уменьшаем количество строк на 1, чтобы пропустить заголовок
 
+            // Считывание данных из файла
             for (int i = 0; i < cols; i++) {
+                int columnRowCount = 0;
                 for (int j = 1; j < rows; j++) { // Начинаем считывание с первой строки после заголовка
                     Row row = sheet.getRow(j);
                     if (row != null) {
                         Cell cell = row.getCell(i);
-                        if (cell != null) {
-                            if (cell.getCellType() == CellType.NUMERIC) {
-                                data[i][j-1] = cell.getNumericCellValue();
-                            } else {
-                                System.out.println("Строка " + (j+1) + ", колонка " + (i+1) + " содержит не число.");
-                                data[i][j-1] = Double.NaN; // Записываем NaN вместо числа
-                            }
+                        if (cell != null && cell.getCellType() == CellType.NUMERIC) {
+                            data[i][j - 1] = cell.getNumericCellValue();
+                            columnRowCount++;
+                        } else {
+                            // Если ячейка не содержит числового значения или пустая, записываем NaN
+                            data[i][j - 1] = Double.NaN;
                         }
+                    } else {
+                        // Если строка отсутствует, записываем NaN
+                        data[i][j - 1] = Double.NaN;
                     }
                 }
+                // Проверка на одинаковое количество строк в каждом столбце
+                if (columnRowCount != rows - 1) {
+                    JOptionPane.showMessageDialog(null, "В столбце " + (i + 1) + " меньшее кол-во строк", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                    return null;
+                }
             }
+
             return data;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
-
 }
-
